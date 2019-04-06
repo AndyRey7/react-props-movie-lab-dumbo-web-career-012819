2206dea3a930ba2c2e94573e2e102f39e4d31980
andy reyes <11710reyes@gmail.com>
2019-04-06 19:56:22 +0000

    Automatically backed up by Learn

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -4,7 +4,8 @@ export default class CardFront extends Component {
 
   render() {
     return (
-      <div class="card-front" style="background-image:url(you spin me right round)"></div>
+      <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
+      </div>
     )
   }
 }
