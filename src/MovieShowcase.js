8414a5a7b634d417a737b835d7e91c1283d36c35
andy reyes <11710reyes@gmail.com>
2019-04-06 19:54:35 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -4,9 +4,9 @@ import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
 
-  generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+    generateMovieCards = () => {
+  		return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
+  	};
 
   render() {
     return (
